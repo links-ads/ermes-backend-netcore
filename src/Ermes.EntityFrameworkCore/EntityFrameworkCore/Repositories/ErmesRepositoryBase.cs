4434f0584872f8d8f36122c943bce7c4f1b925e8
Luca Bruno <luca.bruno@linksfoundation.com>
2021-02-04 15:51:00 +0100

    report bulk repo added

---
+++ src/Ermes.EntityFrameworkCore/EntityFrameworkCore/Repositories/ErmesRepositoryBase.cs
@@ -0,0 +1,30 @@
+﻿using Abp.Domain.Entities;
+using Abp.EntityFrameworkCore;
+using Abp.EntityFrameworkCore.Repositories;
+
+namespace Ermes.EntityFrameworkCore.Repositories
+{
+    //Base class for all repositories in my application
+    public class ErmesRepositoryBase<TEntity, TPrimaryKey> : EfCoreRepositoryBase<ErmesDbContext, TEntity, TPrimaryKey>
+        where TEntity : class, IEntity<TPrimaryKey>
+    {
+        public ErmesRepositoryBase(IDbContextProvider<ErmesDbContext> dbContextProvider)
+            : base(dbContextProvider)
+        {
+        }
+
+        //add common methods for all repositories
+    }
+
+    //A shortcut for entities those have integer Id
+    public class ErmesRepositoryBase<TEntity> : ErmesRepositoryBase<TEntity, int>
+        where TEntity : class, IEntity<int>
+    {
+        public ErmesRepositoryBase(IDbContextProvider<ErmesDbContext> dbContextProvider)
+            : base(dbContextProvider)
+        {
+        }
+
+        //do not add methods here, add them to the class above (because this class inherits it)
+    }
+}
---
+++ src/Ermes.EntityFrameworkCore/EntityFrameworkCore/Repositories/ReportBulkRepository.cs
@@ -0,0 +1,23 @@
+﻿using Abp.EntityFrameworkCore;
+using Ermes.ReportRequests;
+using Ermes.Reports;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Ermes.EntityFrameworkCore.Repositories
+{
+    public class ReportBulkRepository : ErmesRepositoryBase<Report, int>, IReportBulkRepository
+    {
+        public ReportBulkRepository(IDbContextProvider<ErmesDbContext> dbContextProvider)
+            : base(dbContextProvider)
+        {
+        }
+
+        public IQueryable<Report> GetReportsFilteredByReportRequest(ReportRequest rr, int srid)
+        {
+            return Context
+                    .Reports
+                    .FromSqlInterpolated($"SELECT * FROM public.reports r where ST_CONTAINS(ST_GEOMFROMTEXT({rr.AreaOfInterest.ToText()}, {srid}), \"Location\"::geometry) and array(select jsonb_array_elements(r.\"ExtensionData\" ) ->> 'CategoryId')::int[] && {rr.SelectedCategories}");
+        }
+    }
+}
