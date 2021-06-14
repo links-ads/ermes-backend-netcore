6c4ae9a811df1444baa5c9ec1ec821a2638abb8a
Luca Bruno <luca.bruno@linksfoundation.com>
2021-06-14 12:28:02 +0200

    Edited Unique Key of category_translations

---
+++ src/Ermes.EntityFrameworkCore/Migrations/20210614102255_Category_SubGroup_AddedToUniqueKeys.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ermes.Migrations
+{
+    public partial class Category_SubGroup_AddedToUniqueKeys : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_category_translations_Group_Name_Language",
+                table: "category_translations");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_category_translations_Group_SubGroup_Name_Language",
+                table: "category_translations",
+                columns: new[] { "Group", "SubGroup", "Name", "Language" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_category_translations_Group_SubGroup_Name_Language",
+                table: "category_translations");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_category_translations_Group_Name_Language",
+                table: "category_translations",
+                columns: new[] { "Group", "Name", "Language" },
+                unique: true);
+        }
+    }
+}
