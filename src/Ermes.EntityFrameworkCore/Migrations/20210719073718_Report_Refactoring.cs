4110f94574a92844fcb01dbf096a4cbe17776fb9
Luca Bruno <luca.bruno@linksfoundation.com>
2021-07-19 09:46:07 +0200

    report refactoring, useless props removed ref #37

---
+++ src/Ermes.EntityFrameworkCore/Migrations/20210719073718_Report_Refactoring.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Ermes.Reports;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ermes.Migrations
+{
+    public partial class Report_Refactoring : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "reports");
+
+            migrationBuilder.DropColumn(
+                name: "Notes",
+                table: "reports");
+
+            migrationBuilder.DropColumn(
+                name: "Targets",
+                table: "reports");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "reports",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Notes",
+                table: "reports",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<List<ReportTarget>>(
+                name: "Targets",
+                table: "reports",
+                type: "jsonb",
+                nullable: true);
+        }
+    }
+}
