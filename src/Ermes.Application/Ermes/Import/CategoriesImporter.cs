ab00f767d3bbd76d2549c85777890c351ad3bcba
Luca Bruno <luca.bruno@linksfoundation.com>
2021-08-02 10:41:30 +0200

    excelcommon added ref #27

--- src/Ermes.Application/Ermes/Import/CategoriesImporter.cs
+++ src/Ermes.Application/Ermes/Import/CategoriesImporter.cs
@@ -16,6 +16,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Ermes.Excel.Common.ExcelCommon;
 
 namespace Ermes.Import
 {
@@ -23,107 +24,16 @@ namespace Ermes.Import
     {
         public const string IndexSheetName = "Index";
 
-        private interface IMultilanguageCategoryTable
-        {
-            IEnumerable<ICategorySheet> Sheets { get; }
-        }
-
-        private interface ICategorySheet
-        {
-            string Language { get; }
-            IEnumerable<ICategoryRow> Rows { get; }
-        }
-
-        private interface ICategoryRow
-        {
-            int GetInt(string columnName)
-            {
-                return Convert.ToInt32(GetString(columnName) ?? "0");
-            }
-            string GetString(string columnName);
-            string[] GetStringArray(string columnName)
-            {
-                return GetString(columnName)?.Split(',').Select(s => s.Trim()).ToArray();
-            }
-            T GetEnum<T>(string columnName) where T:Enum
-            {
-                return GetString(columnName).ParseEnum<T>();
-            }
-        }
-
-        #region Excel
-        const int numberOfHeaderRows = 1;
-        const int excelIndexingStart = 1;
-        private class ExcelMultilanguageCategoryTable: IMultilanguageCategoryTable
-        {
-            private ExcelWorksheets _excelWorksheets;
-            public ExcelMultilanguageCategoryTable(ExcelWorksheets excelWorksheets)
-            {
-                _excelWorksheets = excelWorksheets;
-            }
-            public IEnumerable<ICategorySheet> Sheets { get { return _excelWorksheets.Select(w => new ExcelCategorySheet(w));  } }
-        }
-
-        private class ExcelCategorySheet: ICategorySheet
-        {
-            private readonly ExcelWorksheet _excelWorksheet;
-            private readonly Dictionary<string, int> _columnsIndexes = new Dictionary<string, int>();
-            public ExcelCategorySheet(ExcelWorksheet excelWorksheet)
-            {
-                _excelWorksheet = excelWorksheet;
-                string colName;
-
-                for (int i = 0; !string.IsNullOrWhiteSpace((colName = _excelWorksheet.Cells[excelIndexingStart, excelIndexingStart + i].Text)); i++)
-                    _columnsIndexes.Add(colName.Trim(), i);
-                    
-            }
-            public string Language { get { return _excelWorksheet.Name; } }
-            public string GetTextForColumn(string columnName, int rowId)
-            {
-                if (!_columnsIndexes.TryGetValue(columnName, out int columnIndex))
-                {
-                    throw new UserFriendlyException($"No such column: {columnName} in sheet {Language}");
-                }
-                string retval = _excelWorksheet.Cells[rowId + numberOfHeaderRows + excelIndexingStart, columnIndex + excelIndexingStart].Text.Trim();
-
-                if (string.IsNullOrWhiteSpace(retval))
-                    return null;
-                else
-                    return retval;
-            }
-            public IEnumerable<ICategoryRow> Rows { 
-                get {
-                    for (int i = 0; !string.IsNullOrWhiteSpace(_excelWorksheet.Cells[i + numberOfHeaderRows + excelIndexingStart, excelIndexingStart].Text); i++)
-                        yield return new ExcelCategoryRow(this, i);
-                } }
-        }
-
-        private class ExcelCategoryRow: ICategoryRow
-        {
-            ExcelCategorySheet _parent;
-            int _index;
-            public ExcelCategoryRow(ExcelCategorySheet parent, int index)
-            {
-                _parent = parent;
-                _index = index;
-            }
-            public string GetString(string columnName)
-            {
-                return _parent.GetTextForColumn(columnName, _index);
-            }            
-        }
-
-        #endregion
         public static async Task<ImportResultDto> ImportCategoriesAsync(string filename, string contentType, CategoryManager categoryManager, ErmesLocalizationHelper localizer, IActiveUnitOfWork context)
         {
-            IMultilanguageCategoryTable categories;
+            IMultilanguageTable categories;
             ImportResultDto result = new ImportResultDto();
             if (contentType == MimeTypeNames.ApplicationVndMsExcel || contentType == MimeTypeNames.ApplicationVndOpenxmlformatsOfficedocumentSpreadsheetmlSheet)
             {
                 FileInfo finfo = new FileInfo(filename);
                 ExcelPackage epack = new ExcelPackage(finfo);
                 epack.Compatibility.IsWorksheets1Based = false;
-                categories = new ExcelMultilanguageCategoryTable(epack.Workbook.Worksheets);
+                categories = new ExcelMultilanguageTable(epack.Workbook.Worksheets);
             }
             else
             {
@@ -131,14 +41,14 @@ namespace Ermes.Import
             }
 
             bool isFirstSheet = true;
-            foreach(ICategorySheet sheet in categories.Sheets)
+            foreach(IErmesSheet sheet in categories.Sheets)
             {
                 if(sheet.Language == IndexSheetName)
                 {
                     if (!isFirstSheet)
                         throw new UserFriendlyException(localizer.L("CategoryImportIndexMustBeFirst"));
 
-                    foreach(ICategoryRow row in sheet.Rows)
+                    foreach(IErmesRow row in sheet.Rows)
                     {
                         Category cat = await categoryManager.GetCategoryByCodeAsync(row.GetString("Code"));
 
@@ -188,7 +98,7 @@ namespace Ermes.Import
                     if (!CultureInfo.GetCultures(CultureTypes.AllCultures).Any(c => c.TwoLetterISOLanguageName == sheet.Language))
                         throw new UserFriendlyException(localizer.L("NotALanguageCode", sheet.Language));
 
-                    foreach (ICategoryRow row in sheet.Rows)
+                    foreach (IErmesRow row in sheet.Rows)
                     {
                         Category parentCat = await categoryManager.GetCategoryByCodeAsync(row.GetString("Code"));
 
