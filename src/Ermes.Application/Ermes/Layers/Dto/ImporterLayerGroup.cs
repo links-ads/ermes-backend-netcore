3768592b3bbc02e355e7e455b2e00df363eb1a3f
Luca Bruno <luca.bruno@linksfoundation.com>
2021-11-10 16:51:22 +0100

    minor

--- src/Ermes.Application/Ermes/Layers/Dto/ImporterLayerGroup.cs
+++ src/Ermes.Application/Ermes/Layers/Dto/ImporterLayerGroup.cs
@@ -6,7 +6,7 @@ namespace Ermes.Layers.Dto
 {
     public class ImporterLayerGroup
     {
-        public int DataTypeId { get; set; }
+        public int DataType_Id { get; set; }
         public List<LayerDetailsDto> Details { get; set; }
     }
 }
