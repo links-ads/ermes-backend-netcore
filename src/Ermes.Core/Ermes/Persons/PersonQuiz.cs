0e19fdf2d95e90610f625a351d0f934234acfab4
Luca Bruno <luca.bruno@linksfoundation.com>
2022-04-26 10:30:06 +0200

    migration added ref #83

---
+++ src/Ermes.Core/Ermes/Persons/PersonQuiz.cs
@@ -0,0 +1,24 @@
+﻿using Abp.Domain.Entities.Auditing;
+using Ermes.Quizzes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Ermes.Persons
+{
+    [Table("person_quizzes")]
+    public class PersonQuiz: CreationAuditedEntity
+    {
+        public PersonQuiz(long personId, string quizCode)
+        {
+            PersonId = personId;
+            QuizCode = quizCode;
+        }
+
+        public virtual Person Person { get; set; }
+        public long PersonId { get; set; }
+        public virtual Quiz Quiz { get; set; }
+        public string QuizCode { get; set; }
+    }
+}
---
+++ src/Ermes.EntityFrameworkCore/Migrations/20220426082410_PersonQuiz_Added.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Ermes.Migrations
+{
+    public partial class PersonQuiz_Added : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "person_quizzes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PersonId = table.Column<long>(nullable: false),
+                    QuizCode = table.Column<string>(nullable: false),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    CreatorUserId = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_person_quizzes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_person_quizzes_persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_person_quizzes_quizzes_QuizCode",
+                        column: x => x.QuizCode,
+                        principalTable: "quizzes",
+                        principalColumn: "Code",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_person_quizzes_QuizCode",
+                table: "person_quizzes",
+                column: "QuizCode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_person_quizzes_PersonId_QuizCode",
+                table: "person_quizzes",
+                columns: new[] { "PersonId", "QuizCode" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "person_quizzes");
+        }
+    }
+}
