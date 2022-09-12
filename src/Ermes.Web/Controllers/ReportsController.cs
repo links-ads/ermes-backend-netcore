199d9b8dd1f005ff884a2c4c1cfbfcddccd28a48
Luca Bruno <luca.bruno@linksfoundation.com>
2022-09-12 16:38:51 +0200

    fix update report API

--- src/Ermes.Web/Controllers/ReportsController.cs
+++ src/Ermes.Web/Controllers/ReportsController.cs
@@ -151,11 +151,11 @@ namespace Ermes.Web.Controllers
                 report.MediaURIs = confirmedMediaURIs.ToList();
             }
 
+            ObjectMapper.Map(reportDto, report);
+
             //citizens' report are public by default
             //TODO: allow user to change this prop from client
             report.IsPublic = _session.Roles != null && _session.Roles.Contains(AppRoles.CITIZEN);
-
-            ObjectMapper.Map(reportDto, report);
 
             //Files to be added are stored in media
             await ManageMedia(report, media, toBeDeleted);
