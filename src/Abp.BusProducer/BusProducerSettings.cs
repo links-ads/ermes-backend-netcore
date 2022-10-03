bb964d8c189f609c00f6f1f3f7935dee3e58e973
Luca Bruno <luca.bruno@linksfoundation.com>
2022-10-03 12:40:27 +0200

    Possbility to disable bus producer from settings added

--- src/Abp.BusProducer/BusProducerSettings.cs
+++ src/Abp.BusProducer/BusProducerSettings.cs
@@ -9,5 +9,6 @@
         public string Exchange { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public bool IsEnabled { get; set; }
     }
 }
