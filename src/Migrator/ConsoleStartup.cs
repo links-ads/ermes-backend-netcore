5bb408301db7af5b1dd9fef0e57bace28bead8c3
lukkia <luca.bruno@linksfoundation.com>
2022-11-09 14:54:50 +0100

    env management in Migrator project ref #117

--- src/Migrator/ConsoleStartup.cs
+++ src/Migrator/ConsoleStartup.cs
@@ -2,6 +2,8 @@
 using Abp.Castle.Logging.Log4Net;
 using Abp.EntityFrameworkCore;
 using Castle.Facilities.Logging;
+using Ermes;
+using Ermes.Configuration;
 using Ermes.EntityFrameworkCore;
 using Ermes.Web.Startup;
 using Microsoft.AspNetCore.Builder;
@@ -16,11 +18,68 @@ namespace Migrator
 {
     public class ConsoleStartup
     {
-        public ConsoleStartup()
+        private readonly IConfigurationRoot _appConfiguration;
+
+        public ConsoleStartup(IWebHostEnvironment env)
         {
+            _appConfiguration = AppConfigurations.Get(env.ContentRootPath, env.EnvironmentName);
+
+            string filename;
+            if (_appConfiguration["App:ErmesProject"] == AppConsts.Ermes_Safers)
+            {
+                switch (_appConfiguration["App:Environment"])
+                {
+                    case "dev":
+                        filename = "appsettings.SafersDevelopment.json";
+                        break;
+                    case "test":
+                        filename = "appsettings.SafersTest.json";
+                        break;
+                    case "local":
+                    default:
+                        filename = "appsettings.json";
+                        break;
+                }
+            }
+            else if(_appConfiguration["App:ErmesProject"] == AppConsts.Ermes_Shelter)
+            {
+                switch (_appConfiguration["App:Environment"])
+                {
+                    case "dev":
+                        filename = "appsettings.ShelterDevelopment.json";
+                        break;
+                    case "test":
+                        filename = "appsettings.ShelterTest.json";
+                        break;
+                    case "local":
+                    default:
+                        filename = "appsettings.json";
+                        break;
+                }
+            }
+            else
+            {
+                switch (_appConfiguration["App:Environment"])
+                {
+                    case "dev":
+                        filename = "appsettings.FasterDevelopment.json";
+                        break;
+                    case "test":
+                        filename = "appsettings.FasterTest.json";
+                        break;
+                    case "prod":
+                        filename = "appsettings.FasterProduction.json";
+                        break;
+                    case "local":
+                    default:
+                        filename = "appsettings.json";
+                        break;
+                }
+            }
+
             Console.WriteLine("Executing builder");
             var builder = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile(filename, optional: true, reloadOnChange: true)
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
         }
@@ -29,7 +88,7 @@ namespace Migrator
         {
             services.AddDbContext<ErmesDbContext>(options =>
             {
-                options.UseNpgsql(Configuration.GetConnectionString("Migrator"),
+                options.UseNpgsql(Configuration.GetConnectionString("Default"),
                     x => x.UseNetTopologySuite(geographyAsDefault: true)
                 );
             });
