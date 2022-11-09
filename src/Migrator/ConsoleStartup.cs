4a3cef4ea19a5a75327034ab4d64cc699ddc5593
lukkia <luca.bruno@linksfoundation.com>
2022-11-09 14:30:43 +0100

    migrator project added ref #117

---
+++ src/Migrator/ConsoleStartup.cs
@@ -0,0 +1,42 @@
+﻿using Abp.AspNetCore;
+using Abp.Castle.Logging.Log4Net;
+using Abp.EntityFrameworkCore;
+using Castle.Facilities.Logging;
+using Ermes.EntityFrameworkCore;
+using Ermes.Web.Startup;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Migrator
+{
+    public class ConsoleStartup
+    {
+        public ConsoleStartup()
+        {
+            Console.WriteLine("Executing builder");
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ErmesDbContext>(options =>
+            {
+                options.UseNpgsql(Configuration.GetConnectionString("Migrator"),
+                    x => x.UseNetTopologySuite(geographyAsDefault: true)
+                );
+            });
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+        }
+    }
+}
---
+++ src/Migrator/Program.cs
@@ -0,0 +1,29 @@
+﻿using Ermes.EntityFrameworkCore;
+using Ermes.Web.Startup;
+using io.fusionauth.domain;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
+
+namespace Migrator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var wh = CreateWebHostBuilder(args).Build();
+            using (var context = (ErmesDbContext)wh.Services.GetService(typeof(ErmesDbContext)))
+            {
+                if (context != null)
+                    context.Database.Migrate();
+            }
+            Console.WriteLine("Done");
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+                WebHost.CreateDefaultBuilder(args)
+                    .UseStartup<ConsoleStartup>();
+    }
+}
