6976a0acac3a7dd806d88dee3ba9df6a84efefab
lukkia <luca.bruno@linksfoundation.com>
2022-11-30 17:17:45 +0100

    fix ExpectedUpdates property

--- src/Ermes.Core/Ermes/MapRequests/MapRequest.cs
+++ src/Ermes.Core/Ermes/MapRequests/MapRequest.cs
@@ -72,7 +72,7 @@ namespace Ermes.MapRequests
         {
             get
             {
-                return Frequency > 0 ? (int)(Duration.UpperBound - Duration.LowerBound).TotalDays / Frequency : 0;
+                return Frequency > 0 ? (int)(Duration.UpperBound - Duration.LowerBound).TotalDays / Frequency : 1;
             }
         }
     }
