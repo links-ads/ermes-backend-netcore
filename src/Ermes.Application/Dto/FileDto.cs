d211279c23b29055f146863677acbd2862f9da1a
lukkia <luca.bruno@linksfoundation.com>
2023-02-17 15:57:25 +0100

    geojson export function added ref #131

---
+++ src/Ermes.Application/Dto/FileDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ermes.Dto
+{
+    public class FileDto
+    {
+        public FileDto() { }
+        public FileDto(string fileName, string fileType)
+        {
+            FileName = fileName;
+            FileType = fileType;
+            FileToken = Guid.NewGuid().ToString("N");
+        }
+
+        public string FileName { get; set; }
+        public string FileType { get; set; }
+        public string FileToken { get; set; }
+    }
+}
