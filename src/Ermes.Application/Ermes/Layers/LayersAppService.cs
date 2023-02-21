1c3d78c793ba7c7be77182570093ee5d5c9cf730
lukkia <luca.bruno@linksfoundation.com>
2023-02-21 09:12:05 +0100

    added comment in getLayers ref #125

--- src/Ermes.Application/Ermes/Layers/LayersAppService.cs
+++ src/Ermes.Application/Ermes/Layers/LayersAppService.cs
@@ -167,6 +167,17 @@ namespace Ermes.Layers
                         )
                     .ToList();
                     result.AssociatedLayers = joinedLayerList.Select(a => ObjectMapper.Map<LayerDto>(a.Layer)).ToList();
+
+                    /*
+                     * It is useless to map all the details about the associated layers, since all the needed information
+                     * can be retrieved through GetTimeSeries API.
+                     
+                        result.AssociatedLayers =
+                            joinedLayerList
+                            .Select(a => new { LayerDto = ObjectMapper.Map<LayerDto>(a.Layer), a.Details })
+                            .Select(a => { a.LayerDto.Details = a.Details; return a.LayerDto; })
+                            .ToList();
+                    */
                 }
                 catch (Exception e)
                 {
