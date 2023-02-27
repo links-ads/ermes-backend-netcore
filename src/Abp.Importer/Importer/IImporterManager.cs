110b94bb94acd3d01fc44f4193bde1bbde1e1ba9
lukkia <luca.bruno@linksfoundation.com>
2023-02-27 14:46:12 +0100

    crs input param added ref #125

--- src/Abp.Importer/Importer/IImporterManager.cs
+++ src/Abp.Importer/Importer/IImporterManager.cs
@@ -8,7 +8,7 @@ namespace Abp.Importer
     public interface IImporterManager
     {
         Task<object> GetLayers(List<string> datatype_ids, string bbox, DateTime start, DateTime end, List<string> request_codes, bool includeMapRequest);
-        Task<object> GetTimeSeries(string datatype_id, string point, string request_code = null, string layer_name = null, DateTime? start = null, DateTime? end = null);
+        Task<object> GetTimeSeries(string datatype_id, string point, string crs, string request_code = null, string layer_name = null, DateTime? start = null, DateTime? end = null);
         Task<object> GetMetadata(string metadata_id);
         Task<List<string>> DeleteMapRequests(List<string> mapRequestCodes);
     }
