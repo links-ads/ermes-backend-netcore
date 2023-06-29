907c705b7338fd6357bcbc00114d785aedc66e90
lukkia <luca.bruno@linksfoundation.com>
2023-06-29 15:32:41 +0200

    logging added ref #145

--- src/Ermes.Core/Ermes/Helpers/ErmesCoreCommon.cs
+++ src/Ermes.Core/Ermes/Helpers/ErmesCoreCommon.cs
@@ -12,18 +12,21 @@ namespace Ermes.Core.Helpers
     {
         public static byte[] CreateThumbnailFromImage(byte[] fileBytes, int thumbnailSize, int imageQuality, ILogger logger)
         {
+            logger.Info($"Original fileBytes size: {fileBytes.Length}");
             logger.Info($"Try to create thumbnail with size: {thumbnailSize}, quality: {imageQuality}");
             using (Image image = Image.FromStream(new MemoryStream(fileBytes)))
             {
                 int minSize = Convert.ToInt32(image.Width > image.Height ? image.Height : image.Width);
                 logger.Info($"Min size: {minSize}");
                 var destRect = new Rectangle(0, 0, thumbnailSize, thumbnailSize);
+                logger.Info($"DestRect created: {destRect.Size}");
                 var destImage = new Bitmap(thumbnailSize, thumbnailSize);
-
+                logger.Info($"DestImage created: {destImage.Size}");
                 destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
-
+                logger.Info($"DestImage setResolution done: {image.HorizontalResolution}, {image.VerticalResolution}");
                 using (var graphics = Graphics.FromImage(destImage))
                 {
+                    logger.Info($"graphics created");
                     graphics.CompositingMode = CompositingMode.SourceCopy;
                     graphics.CompositingQuality = CompositingQuality.HighQuality;
                     graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
@@ -32,8 +35,10 @@ namespace Ermes.Core.Helpers
 
                     using (var wrapMode = new ImageAttributes())
                     {
+                        logger.Info($"Trying to set image attributes....");
                         wrapMode.SetWrapMode(WrapMode.TileFlipXY);
                         graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                        logger.Info($"Image attributes set");
                     }
 
                     EncoderParameters qualityEncodingParameters = new EncoderParameters(1);
