f63bd860011fee9774c30df7b23e719c626150c4
lukkia <luca.bruno@linksfoundation.com>
2023-09-20 17:40:42 +0200

    rel note update

--- src/Ermes.Core/AppVersionHelper.cs
+++ src/Ermes.Core/AppVersionHelper.cs
@@ -15,7 +15,7 @@ namespace Ermes
         /// All project's assembly versions are changed when this value is changed.
         /// It's also shown in the web page.
         /// </summary>
-        public const string Version = "4.14.3";
+        public const string Version = "4.15.0";
 
         /// <summary>
         /// Gets release (last build) date of the application.
