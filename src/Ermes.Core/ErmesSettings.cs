992dde6c2696c063e5491ecc1a728defe9ae8aea
lukkia <luca.bruno@linksfoundation.com>
2023-12-19 10:32:52 +0100

    ermesSettings restored #142

--- src/Ermes.Core/ErmesSettings.cs
+++ src/Ermes.Core/ErmesSettings.cs
@@ -9,9 +9,5 @@ namespace Ermes
         public string CorsOrigins { get; set; }
         public bool HangfireEnabled { get; set; }
         public string ErmesProject { get; set; } = "FASTER";
-        public string WebAppBaseUrl { get; set; }
-        public string WebAppBaseUrlLocal { get; set; }
-        public string AppDomain { get; set; }
-        public string AppDomainLocal { get; set; }
     }
 }
